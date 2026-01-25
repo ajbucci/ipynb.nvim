b72f0b830cc1c5b793773f5e0d76279a038c5661
Ricardo_T8 <ricardoryn1317@gmail.com>
2026-01-25 11:34:02 +0800

    fix(ipynb): update fold fill character configuration

--- ftplugin/ipynb.lua
+++ ftplugin/ipynb.lua
@@ -7,5 +7,5 @@ vim.wo.foldexpr = 'v:lua.require("ipynb.folding").foldexpr()'
 vim.wo.foldtext = 'v:lua.require("ipynb.folding").foldtext()'
 vim.wo.foldenable = true
 vim.wo.foldlevel = 99 -- Start all unfolded
-vim.wo.fillchars = vim.wo.fillchars .. ",fold: " -- Clean fold fill
+vim.opt_local.fillchars:append({ fold = " " }) -- Clean fold fill
 
